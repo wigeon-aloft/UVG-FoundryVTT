e8c6e2da2f452aa00c90b43a446bac84b118eeef
Wigeon <willem@thevanhoofs.com>
2022-10-17 20:55:10 +0100

    Generated base system with generator-foundry

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const UVG = {};
+
+// Define constants here, such as:
+UVG.foobar = {
+  'bas': 'UVG.bas',
+  'bar': 'UVG.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ UVG.abilities = {
+  "str": "UVG.AbilityStr",
+  "dex": "UVG.AbilityDex",
+  "con": "UVG.AbilityCon",
+  "int": "UVG.AbilityInt",
+  "wis": "UVG.AbilityWis",
+  "cha": "UVG.AbilityCha"
+};
+
+UVG.abilityAbbreviations = {
+  "str": "UVG.AbilityStrAbbr",
+  "dex": "UVG.AbilityDexAbbr",
+  "con": "UVG.AbilityConAbbr",
+  "int": "UVG.AbilityIntAbbr",
+  "wis": "UVG.AbilityWisAbbr",
+  "cha": "UVG.AbilityChaAbbr"
+};
